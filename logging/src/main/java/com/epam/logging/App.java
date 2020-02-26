e1c590155fb2af751194e995349f91f2e0760861
priyanka <priyaemani99@gmail.com>
2020-02-26 19:07:05 +0530

    logging

---
+++ logging/src/main/java/com/epam/logging/App.java
@@ -0,0 +1,15 @@
+package com.epam.logging;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOGGER=LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {		
+        LOGGER.info( "Hello World!" );
+    }
+}
